748dbffde9b3b17b91bf7230a8b8730619b9a464
sntactic <khadim.ahmad.mbaye@gmail.com>
2025-11-02 03:02:23 +0000

    suppresion de l'ancienne version du front

--- front/src/app/core/intercepteur/index.ts
+++
@@ -1,6 +0,0 @@
-import { HTTP_INTERCEPTORS } from "@angular/common/http";
-import { AuthIntercepteur } from "./auth_intercepteur";
-
-export const HttpIntercepteurProviders = [
-    {provide : HTTP_INTERCEPTORS , useClass : AuthIntercepteur , multi : true}
-]
