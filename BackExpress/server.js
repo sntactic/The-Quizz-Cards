a9ce7d0ced4bcccbbc027fc20ee4a3f0c6adda26
sntactic <khadim.ahmad.mbaye@gmail.com>
2025-11-06 19:33:48 +0000

    ajout des Dockerfile

--- BackExpress/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app');
-
-
-app.set('port', process.env.PORT || 3000)
-
-const serve  = http.createServer(app);
-
-serve.listen(process.env.PORT || 3000);
